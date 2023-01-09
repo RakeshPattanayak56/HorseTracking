3a3cbe45088e2403c3f04eb91afb4baaec9d87c4
RakeshPattanayak56 <106161927+RakeshPattanayak56@users.noreply.github.com>
2023-01-09 09:48:30 +0530

    Update Program.cs

--- HorseTrack/Program.cs
+++ HorseTrack/Program.cs
@@ -2,10 +2,10 @@
 
 Console.WriteLine("---***Wel Come To Horse Track Programming***---\n");
 Console.WriteLine("---***Enter the action to be performed***---\n");
-Console.WriteLine("\t Press 'A' or 'a' - Inventory and Horses");
-Console.WriteLine("\t Press 'C' or 'c' - Current Inventory ");
+Console.WriteLine("\t Press 'C' or 'c' - Current Inventory and Horses");
 Console.WriteLine("\t Press 'R' or 'r' - Restocks the cash inventory");
 Console.WriteLine("\t Press 'W' or 'w' - Check Horses");
+Console.WriteLine("\t Press 'B' or 'b' - getting into the application");
 Console.WriteLine("\t Press 'Q' or 'q' - Quit the application");
 
 Console.WriteLine("*********************************************");
@@ -15,93 +15,127 @@ string[] horses = { "That Darn Gray Cat", "Fort Utopia", "Count Sheep", "Ms Trai
 int[] odds = { 5, 10, 9, 4, 3, 5, 6 };
 int[] denomination = new int[] { 100,20,10,5,1 };
 int[] inventory1 = { 10, 10, 10, 10, 10 };
-int[] inventory = inventory1;
-
-int amount = 136;
+int[] inventory = { 10, 10, 10, 10, 10 };
+int winner = 1;
 string choice;
-
-
-
-while (true)
+try
 {
-    choice = string.Concat(Console.ReadLine());
-    if (choice == "A" || choice == "R" || choice == "W" || choice == "Q" || choice == "C")
+    while (true)
     {
-        Console.WriteLine();
-    }
-    else if (choice != "A")
-    {
-        Console.WriteLine("\n Invalid Command:{0}", choice);
-    }
-    else if (choice != "W")
-    {
-        Console.WriteLine(" \n Invalid Horse Number:{0}", choice);
-    }
-    else if (choice != "R")
-    {
-        Console.WriteLine(" \n No Payout:{0} ", choice);
-    }
-    else
-    {
-        Console.WriteLine("\n Invalid Command:{0}", choice);
-    }
-
-    switch (choice)
-    {
-        case "C":
-            Console.WriteLine("\n Current Inventory is : {0} ", amount);
-            for (int i = 0; i < denomination.Length; i++)
-            {
-                Console.WriteLine(denomination[i] + "$" + "," + inventory1[i]);
-            }
-            Console.WriteLine("\t\t PLEASE ENTER THE COMMAND");
-            break;
-
-        case "A":
-            Console.WriteLine("\n Inventory :");
-            for (int i = 0; i < denomination.Length; i++)
-            {
-                Console.WriteLine(denomination[i] + "$" + "," + inventory1[i]);
-            }
-            Console.WriteLine("\n Horses: ");
-            for (int i = 0; i < id.Length; i++)
-            {
-                Console.WriteLine(id[i] + "," + horses[i] + "," + odds[i]);
-            }
-            Console.WriteLine("\t\t PLEASE ENTER THE COMMAND");
-            break;
-
-        case "R":
-            for (int i = 0; i < 5; i++)
-            {
-                if (amount >= denomination[i])
-                {
-                    inventory[i] = inventory[i] - ( amount / denomination[i]);
-                    amount = amount % denomination[i];
-                }
-            }
-            Console.WriteLine("Current Inventory", amount);
-            for (int i = 0; i < 5; i++)
-            {
-                    Console.WriteLine(denomination[i]+"$" + "," + inventory[i]);
-            }
-            Console.WriteLine("\t\t PLEASE ENTER THE COMMAND");
-            break;
-
-        case "W":
-            Console.WriteLine("\n Horses: ");
-            for (int i = 0; i < id.Length; i++)
-            {
-                Console.WriteLine(id[i] + "," + horses[i] + "," + odds[i]);
-            }
-            Console.WriteLine("\t\t PLEASE ENTER THE COMMAND");
-            break;
-
-        case "Q":
-            Console.WriteLine("\t\t THANKS FOR USING OUT ATM SERVICE");
+        choice = Console.ReadLine().ToUpper();
+        if (choice == "A" || choice == "R" || choice == "W" || choice == "Q" || choice == "C" || choice == "B")
+        {
+            Console.WriteLine("\n Taking you into the application:{0}", choice);
+        }
+        //else if (choice != "A")
+        //{
+        //    Console.WriteLine("\n Invalid Command:{0}", choice);
+        //}
+        //else if (choice != "W")
+        //{
+        //    Console.WriteLine(" \n Invalid Horse Number:{0}", choice);
+        //}
+        //else if (choice != "R")
+        //{
+        //    Console.WriteLine(" \n No Payout:{0} ", choice);
+        //}
+        else
+        {
+            Console.WriteLine("\n Invalid Command:{0}", choice);
             System.Environment.Exit(0);
-            break;
+        }
 
+        switch (choice)
+        {
+            case "C":
+                PrintHorseAndInventory();
+                break;
+            case "R":
+                Console.WriteLine("Current Inventory: ");                
+                for (int i = 0; i < 5; i++)
+                {
+                    inventory[i] = inventory1[i];
+                    Console.WriteLine(denomination[i] + "$" + "," + inventory[i]);
+                }
+                Console.WriteLine("\t\t PLEASE ENTER THE COMMAND");
+                break;
+
+            case "W":
+                Console.WriteLine("Please enter a number for updating the winner horse: ");
+                winner = Convert.ToInt32(Console.ReadLine());
+                PrintHorseAndInventory();
+                break;
+
+            case "Q":
+                Console.WriteLine("\t\t THANKS FOR USING OUR SERVICE");
+                System.Environment.Exit(0);
+                break;
+            default:
+                Console.WriteLine("Please bet the horse number followed by the bet Amount: ");
+                int expectedWinnerHorse = int.Parse(Console.ReadLine());
+                int betAmount = 0;
+                string betInput = "";
+                try
+                {
+                    betInput = Console.ReadLine();
+                    betAmount = int.Parse(betInput);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("Invalid bet, please enter integer only {0}", betInput);
+                    Console.WriteLine("\t\t PLEASE ENTER THE COMMAND");
+                    break;
+                }
+                if (expectedWinnerHorse == winner)
+                {
+                    int winningAmount = betAmount * odds[winner - 1];
+                    Console.WriteLine("Payout: {0},{1}", horses[expectedWinnerHorse - 1], winningAmount);
+                    Console.WriteLine("Dispensing:");
+                    for (int i = 0; i < 5; i++)
+                    {
+                        if (winningAmount >= denomination[i])
+                        {
+                            inventory[i] = (winningAmount / denomination[i]);
+                            winningAmount = winningAmount % denomination[i];
+                        }
+                        else
+                        {
+                            inventory[i] = 0;
+                        }
+                        Console.WriteLine(denomination[i] + "$" + "," + inventory[i]);
+                    }
+                    Console.WriteLine("Current Inventory", winningAmount);
+                    for (int i = 0; i < 5; i++)
+                    {
+                        inventory[i] = inventory1[i] - inventory[i];
+                        Console.WriteLine(denomination[i] + "$" + "," + inventory[i]);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No Payout: " + horses[expectedWinnerHorse - 1]);
+                }
+                Console.WriteLine("\t\t PLEASE ENTER THE COMMAND");
+                break;
+        }
     }
 }
-
+catch(Exception ex)
+{
+    Console.WriteLine("Invalid command");
+    System.Environment.Exit(0);
+}
+void PrintHorseAndInventory()
+{
+    Console.WriteLine("\n Inventory :");
+    for (int i = 0; i < denomination.Length; i++)
+    {
+        Console.WriteLine(denomination[i] + "$" + "," + inventory[i]);
+    }
+    Console.WriteLine("\n Horses: ");
+    for (int i = 0; i < id.Length; i++)
+    {
+        Console.WriteLine(id[i] + "," + horses[i] + "," + odds[i]);
+    }
+    Console.WriteLine("\t\t PLEASE ENTER THE COMMAND");
+}
